eed17ba154e7ad98fbbadc7471eafdb524769a30
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2020-08-02 23:12:14 +0300

    Add project files.

---
+++ EventsCalendar/Models/Dataset.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventsCalendar.Models
+{
+    public class Dataset
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        [DisplayName("Creation Date")]
+        [DataType(DataType.Date)]
+        public DateTime CreationDate { get; set; }
+
+        public ICollection<Snapshot> Snapshots { get; set; }
+    }
+}
---
+++ EventsCalendar/Models/Snapshot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EventsCalendar.Models
+{
+    public class Snapshot
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Creation Date")]
+        public DateTime CreationDate { get; set; }
+
+
+        public string DatasetID { get; set; }
+        [ForeignKey("DatasetID")]
+        public Dataset Dataset { get; set; }
+
+
+        public ICollection<Campaign> Campaigns { get; set; }
+    }
+}
